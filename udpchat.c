93e74330426ec77f68359d307c1f91c89a3f9f48
DonnellWashington <wasnelly4@gmail.com>
2025-03-14 08:39:18 -0400

    Checking for cla's and added erro functions

--- udpchat.c
+++ udpchat.c
@@ -6,8 +6,39 @@
 #include<sys/socket.h>
 #include"Practical.h"
 
+void DieWithUserMessage(const char *msg, const char *detail){
+    fputs(msg, stderr);
+    fputs(": ", stderr);
+    fputs(detail, stderr);
+    fputc('\n', stderr);
+    exit(1);
+}
+
+void DieWithSystemMessage(const char *msg){
+    perror(msg);
+    exit(1);
+}
+
 int main(int argc, char *argv[]){
 
+    /* So we need to check for the right command line args first 
+       And we know that amount of command line args differ from client to server
+       Server needs to be setup first and needs 1 CLA's (port number)
+       Client needs 3 (Server address, message, port number)
+       So if the cla is 1 then youre the sever and if it has 3 its the client and throw and error otherwsie
+    */
+
+    // Server CLA's
+    if(argc == 1){
+
+    }
+
+    // Client CLA's
+    if (argc == 3){
+        
+    }
+
+    else
     
 
     return 0;
